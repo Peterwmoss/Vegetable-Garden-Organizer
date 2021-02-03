931f3c1a4d949c957cfedcf7377d21159070b3e2
Peter K. Moss <peter@moss.dk>
2021-02-03 14:35:07 +0100

    Added button to start create garden activity
    Should work after merge with feature_creategardenactivity

--- app/src/main/java/dk/mifu/pmos/vegetablegardening/MainActivity.kt
+++ app/src/main/java/dk/mifu/pmos/vegetablegardening/MainActivity.kt
@@ -1,11 +1,19 @@
 package dk.mifu.pmos.vegetablegardening
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val fab: View = findViewById(R.id.fab)
+        fab.setOnClickListener { view ->
+            val createIntent = Intent(this, CreateGardenActivity::class.java)
+            startActivity(createIntent)
+        }
     }
 }
