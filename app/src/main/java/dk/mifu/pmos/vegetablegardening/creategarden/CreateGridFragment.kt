8f2b4e3d84775cc18592b98739e3d71ea1c62a50
miewetkefust <wetkefust@gmail.com>
2021-02-04 18:42:17 +0100

    Buttons now work in specify location fragment

---
+++ app/src/main/java/dk/mifu/pmos/vegetablegardening/creategarden/CreateGridFragment.kt
@@ -0,0 +1,22 @@
+package dk.mifu.pmos.vegetablegardening.creategarden
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import dk.mifu.pmos.vegetablegardening.R
+import kotlinx.android.synthetic.main.fragment_create_grid.*
+
+class CreateGridFragment(private val location: String) : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_create_grid, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        location_text_view.text = location
+    }
+}
--- app/src/main/java/dk/mifu/pmos/vegetablegardening/creategarden/SpecifyLocationFragment.kt
+++ app/src/main/java/dk/mifu/pmos/vegetablegardening/creategarden/SpecifyLocationFragment.kt
@@ -1,11 +1,13 @@
 package dk.mifu.pmos.vegetablegardening.creategarden
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import dk.mifu.pmos.vegetablegardening.R
+import kotlinx.android.synthetic.main.fragment_specify_location.*
 
 class SpecifyLocationFragment: Fragment() {
     override fun onCreateView(
@@ -14,5 +16,17 @@ class SpecifyLocationFragment: Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.fragment_specify_location, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        outdoors_button.setOnClickListener { startCreateGridFragment("Outdoors") }
+        indoors_button.setOnClickListener { startCreateGridFragment("Indoors") }
+        greenhouse_button.setOnClickListener{ startCreateGridFragment("Greenhouse") }
+    }
+
+    private fun startCreateGridFragment(location: String){
+        val createGridFragment = CreateGridFragment(location)
+        fragmentManager?.beginTransaction()?.replace(R.id.fragment_container, createGridFragment)?.commit()
     }
 }
