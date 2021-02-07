7c5b3863f52e6d0e2a5d52ab8414f78b7feffd9e
Peter K. Moss <peter@moss.dk>
2021-02-07 12:59:16 +0100

    Search is now working in choose plant fragment
    Still missing UI fixes for showing the search text and icon

--- app/src/main/java/dk/mifu/pmos/vegetablegardening/creategarden/ChoosePlantFragment.kt
+++ app/src/main/java/dk/mifu/pmos/vegetablegardening/creategarden/ChoosePlantFragment.kt
@@ -1,10 +1,11 @@
 package dk.mifu.pmos.vegetablegardening.creategarden
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.SearchView
+import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import dk.mifu.pmos.vegetablegardening.R
@@ -19,13 +20,33 @@ class ChoosePlantFragment : Fragment() {
                               savedInstanceState: Bundle?): View? {
         val view = inflater.inflate(R.layout.fragment_choose_plant, container, false)
 
-        // Setup recycler view with the plant adapter
-        val recyclerView = view.findViewById<RecyclerView>(R.id.choose_plant_recycler_view)
-        recyclerView.layoutManager = LinearLayoutManager(context)
-        val list = PlantViewModel().plants.toTypedArray()
-        adapter = PlantAdapter(list)
-        recyclerView.adapter = adapter
+        createList(view)
+        setupSearch(view, adapter!!)
 
         return view
+    }
+
+    private fun createList(view: View) {
+        val recyclerView = view.findViewById<RecyclerView>(R.id.choose_plant_recycler_view)
+        recyclerView.layoutManager = LinearLayoutManager(context)
+        val list = PlantViewModel().plants
+        adapter = PlantAdapter(list)
+        recyclerView.adapter = adapter
+    }
+
+    private fun setupSearch(view: View, adapter: PlantAdapter) {
+        val search = view.findViewById<SearchView>(R.id.search_plant_searchview)
+        val listener = object : SearchView.OnQueryTextListener {
+            override fun onQueryTextChange(newText: String?): Boolean {
+                adapter.filter.filter(newText)
+                return false
+            }
+
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                adapter.filter.filter(query)
+                return false
+            }
+        }
+        search.setOnQueryTextListener(listener)
     }
 }
