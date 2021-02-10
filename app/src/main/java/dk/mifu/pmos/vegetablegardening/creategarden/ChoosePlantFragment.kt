27b6a7dd1fb1fa6a93f7d9eb665170f8fff21673
Peter K. Moss <peter@moss.dk>
2021-02-10 17:13:43 +0100

    Optimized imports

--- app/src/main/java/dk/mifu/pmos/vegetablegardening/creategarden/ChoosePlantFragment.kt
+++ app/src/main/java/dk/mifu/pmos/vegetablegardening/creategarden/ChoosePlantFragment.kt
@@ -3,7 +3,6 @@ package dk.mifu.pmos.vegetablegardening.creategarden
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
