f2b83b4c09911df98ba7374eac536e517ad054da
miewetkefust <48484088+miewetkefust@users.noreply.github.com>
2021-02-19 11:34:13 +0100

    Feature/data integration (#66)
    * Can now read from file
    
    * Indoors option removed due to data basis
    
    * plantviewmodel now loads names of all plants in data basis
    
    * plantviewmodel now loads all fields of all plants in data basis
    
    * plantviewmodel now uses live data
    
    * Documentation

--- app/src/main/java/dk/mifu/pmos/vegetablegardening/models/Plant.kt
+++ app/src/main/java/dk/mifu/pmos/vegetablegardening/models/Plant.kt
@@ -1,5 +1,17 @@
 package dk.mifu.pmos.vegetablegardening.models
 
+import java.util.*
+
 data class Plant(
-        val name: String
+        val name: String,
+        val category: String? = null,
+        val earliest: String? = null,
+        val latest: String? = null,
+        val sowing: Boolean? = null,
+        val cropRotation: String? = null,
+        val quantity: String? = null,
+        val sowingDepth: String? = null,
+        val distance: Int? = null,
+        val fertilizer: String? = null,
+        val harvest: String? = null
 )
