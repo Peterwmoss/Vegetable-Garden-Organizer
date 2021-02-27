d71b66efae863383fc7779c4906bbdd59a4c4c7e
Peter K. Moss <peter@moss.dk>
2021-02-27 11:00:54 +0100

    made loading of plant asyncronous

--- app/src/main/java/dk/mifu/pmos/vegetablegardening/viewmodels/PlantViewModel.kt
+++ app/src/main/java/dk/mifu/pmos/vegetablegardening/viewmodels/PlantViewModel.kt
@@ -5,48 +5,57 @@ import android.content.Context
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.viewModelScope
 import com.opencsv.CSVParserBuilder
+import com.opencsv.CSVReaderBuilder
 import dk.mifu.pmos.vegetablegardening.R
 import dk.mifu.pmos.vegetablegardening.models.Plant
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import java.io.*
 import java.util.*
-import com.opencsv.CSVReader
-import com.opencsv.CSVReaderBuilder
 
 class PlantViewModel(application: Application) : AndroidViewModel(application) {
-    val plants: LiveData<MutableList<Plant>> by lazy {
-        loadPlants()
+    lateinit var plants: LiveData<MutableList<Plant>>
+
+    init {
+        viewModelScope.launch {
+            plants = loadPlants()
+        }
     }
 
-    private fun loadPlants(): LiveData<MutableList<Plant>> {
-        val context: Context = getApplication()
+    private suspend fun loadPlants(): LiveData<MutableList<Plant>> {
+        return withContext(Dispatchers.IO) {
+            val context: Context = getApplication()
 
-        val stream = context.resources.openRawResource(R.raw.plantdata)
-        val parser = CSVParserBuilder().withSeparator(';').build()
-        val reader = CSVReaderBuilder(InputStreamReader(stream)).withCSVParser(parser).withSkipLines(1).build()
-        val data = reader.readAll()
+            val stream = context.resources.openRawResource(R.raw.plantdata)
+            val parser = CSVParserBuilder().withSeparator(';').build()
+            val reader = CSVReaderBuilder(InputStreamReader(stream)).withCSVParser(parser).withSkipLines(1).build()
+            val data = reader.readAll()
 
-        val plants: MutableLiveData<MutableList<Plant>> = MutableLiveData()
-        plants.value = mutableListOf()
+            val plants: MutableLiveData<MutableList<Plant>> = MutableLiveData()
+            plants.value = mutableListOf()
 
-        data.forEach { plant ->
-            plants.value?.add(
-                Plant(
-                    name = plant[0],
-                    category = plant[1],
-                    earliest = plant[2],
-                    latest = plant[3],
-                    sowing = plant[4]=="såning",
-                    cropRotation = plant[5],
-                    quantity = plant[6],
-                    sowingDepth = plant[7],
-                    distance = plant[8].toIntOrNull(),
-                    fertilizer = plant[9],
-                    harvest = plant[10]
+            data.forEach { plant ->
+                plants.value?.add(
+                        Plant(
+                                name = plant[0],
+                                category = plant[1],
+                                earliest = plant[2],
+                                latest = plant[3],
+                                sowing = plant[4]=="såning",
+                                cropRotation = plant[5],
+                                quantity = plant[6],
+                                sowingDepth = plant[7],
+                                distance = plant[8].toIntOrNull(),
+                                fertilizer = plant[9],
+                                harvest = plant[10]
+                        )
                 )
-            )
-        }
+            }
 
-        return plants
+            return@withContext plants
+        }
     }
 }
