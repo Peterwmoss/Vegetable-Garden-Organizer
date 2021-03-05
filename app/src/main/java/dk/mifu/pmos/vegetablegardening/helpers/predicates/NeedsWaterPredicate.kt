6dc5e421d166253bdea5e7d12ebd618a0a62a6c7
Peter K. Moss <peter@moss.dk>
2021-03-05 13:48:27 +0100

    local variable to eliminate !!

--- app/src/main/java/dk/mifu/pmos/vegetablegardening/helpers/predicates/NeedsWaterPredicate.kt
+++ app/src/main/java/dk/mifu/pmos/vegetablegardening/helpers/predicates/NeedsWaterPredicate.kt
@@ -5,8 +5,9 @@ import java.util.*
 
 class NeedsWaterPredicate(private val date: Date): Predicate<Plant> {
     override fun invoke(plant: Plant): Boolean {
-        return if (plant.wateredDate != null)
-            plant.wateredDate!! > date
+        val wateredDate = plant.wateredDate
+        return if (wateredDate != null)
+            wateredDate > date
         else {
             true
         }
