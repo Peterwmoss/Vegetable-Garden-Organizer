2ea736ea8be07a54465a6efa7c79cb4a65de197b
miewetkefust <wetkefust@gmail.com>
2021-03-10 13:28:30 +0100

    LocationPredicateTest.kt is done

--- app/src/test/java/dk/mifu/pmos/vegetablegardening/helpers/predicates/LocationPredicateTest.kt
+++ app/src/test/java/dk/mifu/pmos/vegetablegardening/helpers/predicates/LocationPredicateTest.kt
@@ -1,12 +1,41 @@
 package dk.mifu.pmos.vegetablegardening.helpers.predicates
 
+import android.app.Instrumentation
+import android.content.pm.InstrumentationInfo
+import dk.mifu.pmos.vegetablegardening.R
+import dk.mifu.pmos.vegetablegardening.enums.BedLocation
+import dk.mifu.pmos.vegetablegardening.models.Plant
+import org.hamcrest.core.Is.`is`
 import org.junit.Test
 
 import org.junit.Assert.*
 
 class LocationPredicateTest {
 
+    private val greenhouse = "Drivhus"
+    private val outdoors = "Udendørs"
+
     @Test
-    operator fun invoke() {
+    fun invokeGreenhousePlantCorrectLocation() {
+        val locationPredicate = LocationPredicate(BedLocation.Greenhouse)
+        assertThat(locationPredicate.invoke(Plant(name = "Plante1", category = greenhouse)), `is` (true))
+    }
+
+    @Test
+    fun invokeGreenhousePlantWrongLocation() {
+        val locationPredicate = LocationPredicate(BedLocation.Greenhouse)
+        assertThat(locationPredicate.invoke(Plant(name = "Plante2", category = outdoors)), `is` (false))
+    }
+
+    @Test
+    fun invokeOutdoorsPlantCorrectLocation() {
+        val locationPredicate = LocationPredicate(BedLocation.Outdoors)
+        assertThat(locationPredicate.invoke(Plant(name = "Plante3", category = outdoors)), `is` (true))
+    }
+
+    @Test
+    fun invokeOutdoorsPlantWrongLocation() {
+        val locationPredicate = LocationPredicate(BedLocation.Outdoors)
+        assertThat(locationPredicate.invoke(Plant(name = "Plante2", category = greenhouse)), `is` (false))
     }
 }
