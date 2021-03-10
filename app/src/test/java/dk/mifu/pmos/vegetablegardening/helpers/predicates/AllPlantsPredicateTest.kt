7d1585edebbc43236cbefd89750a91513be93933
miewetkefust <wetkefust@gmail.com>
2021-03-10 13:29:31 +0100

    AllPlantsPredicateTest.kt changed from assert to assertThat

--- app/src/test/java/dk/mifu/pmos/vegetablegardening/helpers/predicates/AllPlantsPredicateTest.kt
+++ app/src/test/java/dk/mifu/pmos/vegetablegardening/helpers/predicates/AllPlantsPredicateTest.kt
@@ -2,6 +2,7 @@ package dk.mifu.pmos.vegetablegardening.helpers.predicates
 
 import dk.mifu.pmos.vegetablegardening.models.MyPlant
 import dk.mifu.pmos.vegetablegardening.models.Plant
+import org.hamcrest.core.Is.`is`
 import org.junit.Test
 
 import org.junit.Assert.*
@@ -11,9 +12,9 @@ class AllPlantsPredicateTest {
     @Test
     fun invokeReturnsTrue() {
         val predicate = AllPlantsPredicate()
-        assert(predicate.invoke(Plant("Test")))
-        assert(predicate.invoke(MyPlant("Test")))
-        assert(predicate.invoke(true))
-        assert(predicate.invoke(false))
+        assertThat(predicate.invoke(Plant("Test")), `is`(true))
+        assertThat(predicate.invoke(MyPlant("Test")), `is`(true))
+        assertThat(predicate.invoke(true), `is`(true))
+        assertThat(predicate.invoke(false), `is`(true))
     }
 }
