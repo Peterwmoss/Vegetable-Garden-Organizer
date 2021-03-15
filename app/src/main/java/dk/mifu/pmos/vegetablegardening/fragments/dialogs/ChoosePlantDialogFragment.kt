0fa50ed13aeb335dae2a5c01fa6d4dc6adb6b067
miewetkefust <wetkefust@gmail.com>
2021-03-15 12:00:22 +0100

    Removed unused imports

--- app/src/main/java/dk/mifu/pmos/vegetablegardening/fragments/dialogs/ChoosePlantDialogFragment.kt
+++ app/src/main/java/dk/mifu/pmos/vegetablegardening/fragments/dialogs/ChoosePlantDialogFragment.kt
@@ -3,7 +3,6 @@ package dk.mifu.pmos.vegetablegardening.fragments.dialogs
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -14,22 +13,19 @@ import android.widget.Filterable
 import android.widget.TextView
 import androidx.fragment.app.DialogFragment
 import androidx.fragment.app.activityViewModels
-import androidx.navigation.findNavController
-import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import dk.mifu.pmos.vegetablegardening.R
 import dk.mifu.pmos.vegetablegardening.databinding.FragmentChoosePlantBinding
-import dk.mifu.pmos.vegetablegardening.fragments.viewgarden.BedOverviewFragmentDirections
-import dk.mifu.pmos.vegetablegardening.viewmodels.BedViewModel
 import dk.mifu.pmos.vegetablegardening.helpers.predicates.LocationPredicate
 import dk.mifu.pmos.vegetablegardening.models.MyPlant
 import dk.mifu.pmos.vegetablegardening.models.Plant
+import dk.mifu.pmos.vegetablegardening.viewmodels.BedViewModel
 import dk.mifu.pmos.vegetablegardening.viewmodels.PlantViewModel
 import java.util.*
 
-class ChoosePlantDialogFragment() : DialogFragment() {
+class ChoosePlantDialogFragment : DialogFragment() {
     private lateinit var binding: FragmentChoosePlantBinding
 
     private val plantViewModel: PlantViewModel by activityViewModels()
