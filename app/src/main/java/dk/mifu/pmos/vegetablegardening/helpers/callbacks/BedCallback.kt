c36ce28a661538999a42715633fcc3d92a013096
miewetkefust <wetkefust@gmail.com>
2021-03-17 17:19:42 +0100

    Bug fixed

--- app/src/main/java/dk/mifu/pmos/vegetablegardening/helpers/callbacks/BedCallback.kt
+++ app/src/main/java/dk/mifu/pmos/vegetablegardening/helpers/callbacks/BedCallback.kt
@@ -3,15 +3,25 @@ package dk.mifu.pmos.vegetablegardening.helpers.callbacks
 import android.view.View
 import android.widget.Button
 import androidx.databinding.ObservableMap
+import androidx.navigation.findNavController
+import dk.mifu.pmos.vegetablegardening.fragments.viewgarden.BedOverviewFragmentDirections
 import dk.mifu.pmos.vegetablegardening.models.Coordinate
 import dk.mifu.pmos.vegetablegardening.models.MyPlant
 import dk.mifu.pmos.vegetablegardening.viewmodels.BedViewModel
 
-class BedCallback(private val view: View, private val bedViewModel: BedViewModel): BedPlantsChangedCallback() {
+class BedCallback(
+        private val view: View,
+        private val bedViewModel: BedViewModel): BedPlantsChangedCallback() {
+
     override fun onMapChanged(sender: ObservableMap<Coordinate, MyPlant>?, key: Coordinate?) {
         val id = bedViewModel.tileIds?.get(key)!!
         val button = view.findViewById<Button>(id)
-        if(button != null)
-            button.text = bedViewModel.plants?.get(key)?.name
+        if(button != null){
+            val plant = bedViewModel.plants?.get(key)
+            button.text = plant?.name
+            button.setOnClickListener { _ ->
+                view.findNavController().navigate(BedOverviewFragmentDirections.showPlantInfo(key!!, plant!!))
+            }
+        }
     }
 }
